279ee89d03550ef11c35e660f6dcb5eb9b6b0ff6
wardmanc <wardmanc@amazon.com>
2021-01-05 14:31:10 -0500

    Added some basic checks to json type casting

--- Json.h
+++ Json.h
@@ -10,5 +10,15 @@
 #include "JsonNull.h"
 #include "JsonDriver.h"
 
+#define CHECK_ASSIGN_TYPE(dst,src,type)    (((((dst)=((src)->getType()==type)?dynamic_cast<typeof(dst)>(src):NULL))==NULL)?-1:0)
+#define CHECK_ASSIGN_SRCNULL(dst, src,type) ((src==NULL)?-1:CHECK_ASSIGN_TYPE(dst,src,type))
+#define CHECK_ASSIGN_OBJECT(dst, src)       CHECK_ASSIGN_SRCNULL(dst,src, JSON_OBJECT)
+#define CHECK_ASSIGN_ARRAY(dst, src)        CHECK_ASSIGN_SRCNULL(dst,src, JSON_ARRAY)                    
+#define CHECK_ASSIGN_NUMBER(dst, src)       CHECK_ASSIGN_SRCNULL(dst,src, JSON_NUMBER)
+#define CHECK_ASSIGN_STRING(dst, src)       CHECK_ASSIGN_SRCNULL(dst,src, JSON_STRING)
+#define CHECK_ASSIGN_NULL(dst, src)        CHECK_ASSIGN_SRCNULL(dst,src, JSON_NULL)
+#define CHECK_ASSIGN_BOOL(dst, src)        CHECK_ASSIGN_SRCNULL(dst,src, JSON_BOOL)
+
+
 #endif
 
